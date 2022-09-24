44bac1d86d7d95ce9c7c8ae09718b18228b10667
IsmailMusa-clo <75386858+IsmailMusa-clo@users.noreply.github.com>
2022-09-24 12:23:13 +0300

    add files

---
+++ BirdGame/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace BirdGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlGame = new System.Windows.Forms.Panel();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.bieDown1 = new System.Windows.Forms.PictureBox();
+            this.bie2 = new System.Windows.Forms.PictureBox();
+            this.bieDown = new System.Windows.Forms.PictureBox();
+            this.bie1 = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlGame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bieDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bie2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bieDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bie1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlGame
+            // 
+            this.pnlGame.BackColor = System.Drawing.Color.BlueViolet;
+            this.pnlGame.Controls.Add(this.lblGameOver);
+            this.pnlGame.Controls.Add(this.bieDown1);
+            this.pnlGame.Controls.Add(this.bie2);
+            this.pnlGame.Controls.Add(this.bieDown);
+            this.pnlGame.Controls.Add(this.bie1);
+            this.pnlGame.Controls.Add(this.ground);
+            this.pnlGame.Controls.Add(this.bird);
+            this.pnlGame.Location = new System.Drawing.Point(-2, 51);
+            this.pnlGame.Name = "pnlGame";
+            this.pnlGame.Size = new System.Drawing.Size(898, 562);
+            this.pnlGame.TabIndex = 0;
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.BackColor = System.Drawing.Color.DimGray;
+            this.lblGameOver.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblGameOver.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Bold);
+            this.lblGameOver.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblGameOver.Location = new System.Drawing.Point(289, 170);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(273, 45);
+            this.lblGameOver.TabIndex = 2;
+            this.lblGameOver.Text = "Game Over....";
+            this.lblGameOver.Visible = false;
+            this.lblGameOver.Click += new System.EventHandler(this.lblGameOver_Click);
+            // 
+            // bieDown1
+            // 
+            this.bieDown1.BackColor = System.Drawing.Color.Transparent;
+            this.bieDown1.Image = global::BirdGame.Properties.Resources.pipedown;
+            this.bieDown1.Location = new System.Drawing.Point(483, 0);
+            this.bieDown1.Name = "bieDown1";
+            this.bieDown1.Size = new System.Drawing.Size(105, 153);
+            this.bieDown1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bieDown1.TabIndex = 5;
+            this.bieDown1.TabStop = false;
+            // 
+            // bie2
+            // 
+            this.bie2.BackColor = System.Drawing.Color.Transparent;
+            this.bie2.Image = global::BirdGame.Properties.Resources.pipe;
+            this.bie2.Location = new System.Drawing.Point(793, 334);
+            this.bie2.Name = "bie2";
+            this.bie2.Size = new System.Drawing.Size(105, 137);
+            this.bie2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bie2.TabIndex = 4;
+            this.bie2.TabStop = false;
+            // 
+            // bieDown
+            // 
+            this.bieDown.BackColor = System.Drawing.Color.Transparent;
+            this.bieDown.Image = global::BirdGame.Properties.Resources.pipedown;
+            this.bieDown.Location = new System.Drawing.Point(122, 0);
+            this.bieDown.Name = "bieDown";
+            this.bieDown.Size = new System.Drawing.Size(105, 153);
+            this.bieDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bieDown.TabIndex = 3;
+            this.bieDown.TabStop = false;
+            // 
+            // bie1
+            // 
+            this.bie1.BackColor = System.Drawing.Color.Transparent;
+            this.bie1.Image = global::BirdGame.Properties.Resources.pipe;
+            this.bie1.Location = new System.Drawing.Point(689, 334);
+            this.bie1.Name = "bie1";
+            this.bie1.Size = new System.Drawing.Size(105, 137);
+            this.bie1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bie1.TabIndex = 2;
+            this.bie1.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = global::BirdGame.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(0, 466);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(903, 96);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 1;
+            this.ground.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.Image = global::BirdGame.Properties.Resources.bird;
+            this.bird.Location = new System.Drawing.Point(72, 288);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(79, 74);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Bold);
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(115, 33);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "score:0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(894, 612);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pnlGame);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bird game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.pnlGame.ResumeLayout(false);
+            this.pnlGame.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bieDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bie2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bieDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bie1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlGame;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox bie2;
+        private System.Windows.Forms.PictureBox bieDown;
+        private System.Windows.Forms.PictureBox bie1;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox bieDown1;
+        private System.Windows.Forms.Label lblGameOver;
+    }
+}
+
---
+++ BirdGame/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BirdGame
+{
+    public partial class Form1 : Form
+    {
+
+        int Speed = 8;
+        int score = 0;
+        int gravey = 6;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bird.Top += gravey; 
+            bie1.Left -= Speed;
+            bie2.Left -= Speed;
+            bieDown.Left -= Speed;
+            bieDown1.Left -= Speed;
+            score++;
+             lblScore.Text = "Score:" + score/2;
+            for (int i = 0; i <10000; i+=100)
+            {
+                if (score/2 == i) { Speed +=2; }
+            }
+            if (bie1.Left < -bieDown.Width)
+            {
+                bie1.Visible = false;
+                if (bie1.Left != bie2.Left)
+                { bie1.Left += r.Next(pnlGame.Width, pnlGame.Width * 2);
+                    bie1.Height = r.Next(120, 150);
+                }
+                else
+                    bie1.Left += pnlGame.Width;
+
+                bie1.Visible = true;
+            }
+            if (bie2.Left < -bieDown.Width)
+            {
+                bie2.Visible = false;
+                bie2.Left += r.Next(pnlGame.Width, pnlGame.Width * 2);
+                bie2.Height = r.Next(120, 150);
+                bie2.Visible = true;
+            }
+            if (bieDown.Left <-bieDown.Width)
+            {
+                bieDown.Visible = false;
+                if (bieDown.Left != bieDown1.Left)
+                { bieDown.Left += r.Next(pnlGame.Width, pnlGame.Width * 2);
+                    bieDown.Height = r.Next(130, 170);
+                }
+                else
+                    bieDown.Left += pnlGame.Width; 
+
+                bieDown.Visible = true;
+            }
+            if (bieDown1.Left < -bieDown.Width)
+            {
+                bieDown1.Visible = false;
+                bieDown1.Left += r.Next(pnlGame.Width, pnlGame.Width * 2);
+                bieDown1.Height = r.Next(130, 170);
+                bieDown1.Visible = true;
+            }
+
+            if(bird.Bounds.IntersectsWith(bie1.Bounds)|| bird.Bounds.IntersectsWith(bie2.Bounds) ||
+                bird.Bounds.IntersectsWith(bieDown.Bounds) || bird.Bounds.IntersectsWith(bieDown1.Bounds) 
+                ||bird.Top<0||bird.Top>pnlGame.Height-bird.Height)
+            {
+                timer1.Enabled = false;
+                lblGameOver.Visible = true;
+
+            }
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Space)
+            {
+                gravey = -6; 
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                                score = 0;
+                this.bie2.Left = 793;
+                this.bieDown.Location = new System.Drawing.Point(122, 0);
+                this.bie1.Left = 689;
+                this.bird.Location = new System.Drawing.Point(72, 288);
+                this.bieDown1.Location = new System.Drawing.Point(482, 0);
+                lblGameOver.Visible = false;
+                timer1.Enabled = true;
+
+            }
+
+        }
+            private void Form1_KeyUp(object sender, KeyEventArgs e)
+            {
+
+         
+           
+                gravey = 6;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGameOver_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
